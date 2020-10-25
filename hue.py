778d42a86c7a53fde3dc640f20e4f3588e364829
HisayaSugita( kino ) <pswty180@gmail.com>
2020-10-26 00:20:21 +0900

    dotenv導入

--- hue.py
+++ hue.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import requests
 import time
+import settings
 
-HUE_API = 'http://192.168.1.2/api/bCQvM-LF70eAr6TDQtKCE3qxiu5x7BCSWEDgfbDH/lights'
+HUE_API_PATH = settings.HUE_API_PATH
 
-requests.put(HUE_API + '/4/state',
+requests.put(HUE_API_PATH + '/4/state',
              json={"on": True, "bri": 128, "xy": [0.48, 0.41]})
 
 time.sleep(5)
 
-requests.put(HUE_API + '/4/state', json={"on": False})
+requests.put(HUE_API_PATH + '/4/state', json={"on": False})
