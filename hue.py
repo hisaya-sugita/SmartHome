b453b4444a365dd710f04aef316749091fec9b0a
HisayaSugita( kino ) <pswty180@gmail.com>
2020-10-26 01:24:51 +0900

    全ての照明グループを一斉に操作するようにする

--- hue.py
+++ hue.py
@@ -5,9 +5,12 @@ import settings
 
 HUE_API_PATH = settings.HUE_API_PATH
 
-requests.put(HUE_API_PATH + '/lights/4/state',
-             json={"on": True, "bri": 128, "xy": [0.48, 0.41]})
+for i in range(10):
+    requests.put(HUE_API_PATH + "/groups/" + str(i) + "/action",
+                json={"on": False})
 
 time.sleep(5)
 
-requests.put(HUE_API_PATH + '/lights/4/state', json={"on": False})
+for i in range(10):
+    requests.put(HUE_API_PATH + "/groups/" + str(i) + "/action",
+                json={"on": True, "bri": 255, "ct": 255})
