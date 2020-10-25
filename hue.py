6d863da15bbd3777f45ca3c836596f3b7191912c
HisayaSugita( kino ) <pswty180@gmail.com>
2020-10-26 00:28:34 +0900

    微修正

--- hue.py
+++ hue.py
@@ -5,9 +5,9 @@ import settings
 
 HUE_API_PATH = settings.HUE_API_PATH
 
-requests.put(HUE_API_PATH + '/4/state',
+requests.put(HUE_API_PATH + '/lights/4/state',
              json={"on": True, "bri": 128, "xy": [0.48, 0.41]})
 
 time.sleep(5)
 
-requests.put(HUE_API_PATH + '/4/state', json={"on": False})
+requests.put(HUE_API_PATH + '/lights/4/state', json={"on": False})
